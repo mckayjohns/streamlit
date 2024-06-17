aae771de0eb72607a28f16a94e954591dd9f28f6
mckay-luz <mckay@luzdata.com>
2024-06-17 15:28:24 -0600

    updates

--- streamlit_euros/shot_map_filter.py
+++ streamlit_euros/shot_map_filter.py
@@ -5,6 +5,7 @@ import streamlit as st
 
 from mplsoccer import VerticalPitch, Pitch
 
+
 def scrape_sofascore(url: str) -> None:
 
     match_id = url.split('#id:')[-1]
@@ -38,6 +39,7 @@ def scrape_sofascore(url: str) -> None:
     # Append the data to the shots.csv
     df.to_csv('~/Documents/Github/streamlit/streamlit_euros/shots.csv', mode='a', header=False, index=False)
 
+
 # scrape_sofascore('https://www.sofascore.com/germany-scotland/VTbslUb#id:11873905')
 # scrape_sofascore('https://www.sofascore.com/hungary-switzerland/ZTbsjUb#id:11873897')
 # scrape_sofascore('https://www.sofascore.com/croatia-spain/YTbspUb#id:11873907')
@@ -45,6 +47,9 @@ def scrape_sofascore(url: str) -> None:
 # scrape_sofascore('https://www.sofascore.com/netherlands-poland/dUbsfUb#id:12198302')
 # scrape_sofascore(url='https://www.sofascore.com/slovenia-denmark/BObsJOb#id:11873971')
 # scrape_sofascore('https://www.sofascore.com/serbia-england/nUbsfCc#id:11873969')
+# scrape_sofascore('https://www.sofascore.com/austria-france/GObstUb#id:11873975')
+# scrape_sofascore('https://www.sofascore.com/ukraine-romania/CObsbUb#id:12198226')
+# scrape_sofascore('https://www.sofascore.com/belgium-slovakia/XTbsrUb#id:11873978')
 
 
 st.title("Euros 2024 Shot Map")
@@ -53,6 +58,7 @@ st.subheader("Filter to any team/player to see all their shots taken!")
 # Load the data
 try:
     df = pd.read_csv('./streamlit_euros/shots.csv')
+    # df = pd.read_csv('~/Documents/Github/streamlit/streamlit_euros/shots.csv')
     df = df[df['situation'] != 'own'].reset_index()
     df = df.sort_values(by=['team', 'player'])
 except pd.errors.EmptyDataError:
@@ -101,4 +107,4 @@ plot_shots(filtered_df, ax, pitch)
 st.pyplot(fig)
 
 
-st.text("Last Updated: 2024-06-19")
+st.text("Last Updated: 2024-06-20")
